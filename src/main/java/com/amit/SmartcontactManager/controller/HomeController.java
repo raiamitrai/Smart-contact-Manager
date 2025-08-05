8c494f17f3d265bd9aa220ee0a3888008aa4020c
Amit Kumar Rai <raiamitrai1001@gmail.com>
2025-08-06 01:53:28 +0530

    Add Home and About pages

--- src/main/java/com/amit/SmartcontactManager/controller/HomeController.java
+++ src/main/java/com/amit/SmartcontactManager/controller/HomeController.java
@@ -1,20 +1,42 @@
 package com.amit.SmartcontactManager.controller;
 
+import com.amit.SmartcontactManager.entities.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("title" , "Home - Smart Contact Manager");
         return "home";
     }
 
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+        model.addAttribute("title" , "About - Smart Contact Manager");
         return "about";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("title" , "Signup - Smart Contact Manager");
+        model.addAttribute("user" , new User());
+        return "signUp";
+    }
+
+    // this hander for registering user
+    @PostMapping("/do_register")
+    public String registeruser(@ModelAttribute("user") User user  , @RequestParam(value = "agreement" , defaultValue = "false") boolean agreement , Model model){
+        System.out.println("Agreement" + agreement);
+        System.out.println("user" + user);
+        return "signUp";
     }
 
 }
