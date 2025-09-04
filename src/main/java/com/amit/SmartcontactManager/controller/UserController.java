ed3fd7b968233ef8ab1cef6769a58c7c4393621d
Amit Kumar Rai <raiamitrai1001@gmail.com>
2025-09-05 04:37:27 +0530

    upload image , Integrating text editor

--- src/main/java/com/amit/SmartcontactManager/controller/UserController.java
+++ src/main/java/com/amit/SmartcontactManager/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.amit.SmartcontactManager.controller;
 
 import com.amit.SmartcontactManager.entities.Contact;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ui.Model;
 import com.amit.SmartcontactManager.entities.User;
 import com.amit.SmartcontactManager.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 @Controller
@@ -44,19 +48,57 @@ public class UserController {
     }
 
     @PostMapping("/process-contact")
-    public String processContact(@ModelAttribute Contact contact, Principal principal) {
-        // 1. Current logged-in user nikalna
-        String userName = principal.getName();
-        User user = userRepository.getUserByUserName(userName);
+    public String processContact(
+            @ModelAttribute Contact contact, Model model,
+            @RequestParam("profileImage")
+            MultipartFile file, Principal principal) {
 
-        // 2. Contact ko us user ke saath link karna
-        contact.setUser(user);
-        user.getContacts().add(contact);
+        try{
+            // 1. Current logged-in user nikalna
+            String userName = principal.getName();
+            User user = userRepository.getUserByUserName(userName);
 
-        // 3. User save karne se contact bhi save ho jayega
-        userRepository.save(user);
+            //processing and uploading file
+            if(file.isEmpty()){
+                // if the file is empty the try oue message;
+                System.out.println("File is empty");
+            }
+            else{
+                //upload file
+                contact.setImage(file.getOriginalFilename());
 
+                File saveFile = new ClassPathResource("static/img").getFile();
+                Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+
+                Files.copy(file.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
+
+                System.out.println("file is uploded");
+            }
+
+            // 2. Contact ko us user ke saath link karna
+            contact.setUser(user);
+            user.getContacts().add(contact);
+
+            // 3. User save karne se contact bhi save ho jayega
+            userRepository.save(user);
+
+            model.addAttribute("message", "✅ Contact added successfully!");
+            model.addAttribute("alertType", "success");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+
+            // Error case
+            model.addAttribute("message", "❌ Something went wrong!");
+            model.addAttribute("alertType", "danger");
+        }
         return "normal/add_contact_form";
+    }
+
+    // view contact handler
+    @GetMapping("/contacts")
+    public String viewContact(){
+        return "normal/view_contacts";
     }
 
 }
